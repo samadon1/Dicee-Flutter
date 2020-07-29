418ffc80eb608c70f866413e323c637324a78d2a
samadon1 <calvinsamueldonkor@gmail.com>
2020-07-28 22:44:42 -0700

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -9,51 +11,56 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 80.0,
-              backgroundImage: AssetImage("images/me1.jpg"),
-            ),
-            Text('Samuel A Donkor' , style: TextStyle(fontFamily: 'Pacifico',fontSize: 30.0,
-                color: Colors.white, fontWeight: FontWeight.bold),),
-            Text('Flutter Developer', style: TextStyle(fontFamily: 'Source Sans Pro', fontWeight: FontWeight.bold, fontSize: 20.0,
-              color: Colors.indigo[100], letterSpacing: 5.0
-            ),),
-            SizedBox(
-              height: 30.0,
-              width: 200.0,
-              child: Divider(
-                color: Colors.white,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30.0, right: 30.0),
-              child: Card(
-                child: ListTile(
-                  leading: Icon(Icons.call, color: Colors.indigo),
-                  title: Text('+233 123 456 789', style: TextStyle(fontSize:15.0, fontFamily: 'Source Sans Pro',
-                    fontWeight: FontWeight.bold
-                  ),),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30.0, right: 30.0),
-              child: Card(
-                child: ListTile(
-                  leading: Icon(Icons.email, color: Colors.indigo),
-                  title: Text('samueladonkor@gmail.com', style: TextStyle(fontSize: 15.0 ,fontFamily: 'Source Sans pro',
-                    fontWeight: FontWeight.bold
-                  ),),
-                ),
-              ),
-            )
-          ],
-        )
-      ) ,
+        backgroundColor: Colors.purple,
+        appBar: AppBar(
+          title: Text(
+            'Dicee',
+            style: TextStyle(color: Colors.amber,
+                fontFamily: 'Pacifico',
+                fontSize: 37.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Page(),
+      ),
+    );
+  }
+}
+
+class Page extends StatefulWidget {
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  int rightDiceNumber = 1;
+  int leftDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      // ignore: unnecessary_statements
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image(image: AssetImage('images/dice$leftDiceNumber.png'),color: Colors.white,))),
+          Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                    // ignore: unnecessary_statements
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                  child: Image(image: AssetImage('images/dice$rightDiceNumber.png'), color: Colors.white,))),
+        ],
+      ),
     );
   }
 }
